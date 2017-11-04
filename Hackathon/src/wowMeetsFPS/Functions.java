0766a0879b818a885d94eeb031206a0793e91ce1
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2017-11-04 16:53:55 -0400

    Finished Function
    Fixed syntax errors

--- Hackathon/src/wowMeetsFPS/Functions.java
+++ Hackathon/src/wowMeetsFPS/Functions.java
@@ -5,16 +5,19 @@ import java.awt.geom.Point2D;
 public class Functions {
 
 	public boolean collides(Point2D.Double aTL, Point2D.Double aBR, Point2D.Double bTL, Point2D.Double bBR) {
-		/*if(aTL.getX().compareTo(bBR.getX()) > 0 || bTL.getX().compareTo(aBR.getX()) > 0){
+		Double aTLX = aTL.getX(), aTLY = aTL.getY();
+		Double aBRX = aBR.getX(), aBRY = aBR.getY();
+		Double bTLX = bTL.getX(), bTLY = bTL.getY();
+		Double bBRX = bBR.getX(), bBRY = bBR.getY();
+		
+		if(aTLX.compareTo(bBRX) > 0 || bTLX.compareTo(aBRX) > 0){
 			
 			return false;
 		}
-		if(aTL.getY().compareTo(bBR.getY()) > 0 || bTL.getY().compareTo(aBR.getY()) > 0){
+		if(aTLY.compareTo(bBRY) > 0 || bTLY.compareTo(aBRY) > 0){
 			
 			return false;
 		}
 		return true;
-		*/
-		return false;
 	}
 }
