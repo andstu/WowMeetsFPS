e20816fcd1f34c521d6f7a38570624830865ef2c
andstu <atlightning11@gmail.com>
2017-11-04 02:11:00 -0400

    ItemInterface

--- Hackathon/src/wowMeetsFPS/Item.java
+++ Hackathon/src/wowMeetsFPS/Item.java
@@ -1,5 +1,13 @@
 package wowMeetsFPS;
 
+import javax.swing.ImageIcon;
+
 public interface Item {
 
+	public int getHpBonus();
+	public int getAttackBonus();
+	public int getSpeedBonus();
+	public int getDefenseBonus();
+	public ImageIcon getImage();
+	
 }
