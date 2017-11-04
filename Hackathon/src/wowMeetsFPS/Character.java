95a329b01cb93a63512e1d279d09bed30e44a7f5
andstu <atlightning11@gmail.com>
2017-11-04 16:46:48 -0400

    Tank

--- Hackathon/src/wowMeetsFPS/Character.java
+++ Hackathon/src/wowMeetsFPS/Character.java
@@ -20,22 +20,21 @@ public interface Character {
 	 * Should have a linklist to store projectiles
 	 * cooldown so no spamming
 	 */
-	public void attack(Point2D target);
+	public void attack(Point2D.Double target);
 	
 	public int getHP();
 	public int getDefense();
 	public int getAttack();
 	public int getSpeed();
 	public ImageIcon getImage();
-	public Point.Double getLoc();
+	public Point2D.Double getLoc();
 	
 	public void setHP(int hp);
 	public void setDefense(int defense);
 	public void setAttack(int attack);
 	public void setSpeed(int speed);
-	public ImageIcon setImage(ImageIcon sprite);
-	public Point.Double setLoc(Point.Double location);
+	public void setImage(ImageIcon sprite);
+	public void setLoc(Point2D.Double location);
 	
 	public String getID();
-	public void setID(String id);
 }
--- Hackathon/src/wowMeetsFPS/Tank.java
+++ Hackathon/src/wowMeetsFPS/Tank.java
@@ -1,44 +1,60 @@
 package wowMeetsFPS;
 
-import java.awt.geom.Point2D.Double;
+import java.awt.geom.Point2D;
 
 import javax.swing.ImageIcon;
 
 public class Tank implements Character {
-
-	@Override
-	public void move() {
-		// TODO Auto-generated method stub
+	private Point2D.Double location;
+	private ImageIcon image;
+	private String id;
+	private int defense, attack, hp, speed;
+	
+	public Tank(String id) {
+		this.id = id;
 	}
 
 	@Override
-	public void attack() {
+	public void move(Dir dir) {
 		// TODO Auto-generated method stub
+		int x = 0, y = 0;
+		switch(dir) {
+		case UP: y = speed; break;
+		case DOWN: y = -1 * speed; break;
+		case LEFT: x = -1 * speed; break;
+		case RIGHT: x = speed; break;
+		}
+		location.setLocation(location.getX() + x, location.getY() + y);
+	}
 
+	@Override
+	public void attack(Point2D.Double target) {
+		// TODO Auto-generated method stub
+		new Projectile(50, attack, 10, location, target);
 	}
 
 	@Override
 	public int getHP() {
 		// TODO Auto-generated method stub
-		return 0;
+		return hp;
 	}
 
 	@Override
 	public int getDefense() {
 		// TODO Auto-generated method stub
-		return 0;
+		return defense;
 	}
 
 	@Override
 	public int getAttack() {
 		// TODO Auto-generated method stub
-		return 0;
+		return attack;
 	}
 
 	@Override
 	public int getSpeed() {
 		// TODO Auto-generated method stub
-		return 0;
+		return speed;
 	}
 
 	@Override
@@ -48,57 +64,51 @@ public class Tank implements Character {
 	}
 
 	@Override
-	public Double getLoc() {
+	public Point2D.Double getLoc() {
 		// TODO Auto-generated method stub
-		return null;
+		return location;
 	}
 
 	@Override
 	public void setHP(int hp) {
 		// TODO Auto-generated method stub
-
+		this.hp = hp;
 	}
 
 	@Override
 	public void setDefense(int defense) {
 		// TODO Auto-generated method stub
-
+		this.defense = defense;
 	}
 
 	@Override
 	public void setAttack(int attack) {
 		// TODO Auto-generated method stub
-
+		this.attack = attack;
 	}
 
 	@Override
 	public void setSpeed(int speed) {
 		// TODO Auto-generated method stub
-
+		this.speed = speed;
 	}
 
 	@Override
-	public ImageIcon setImage(ImageIcon sprite) {
+	public void setImage(ImageIcon sprite) {
 		// TODO Auto-generated method stub
-		return null;
+		this.image = sprite;
 	}
 
 	@Override
-	public Double setLoc(Double location) {
+	public void setLoc(Point2D.Double location) {
 		// TODO Auto-generated method stub
-		return null;
+		this.location = location;
 	}
 
 	@Override
 	public String getID() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public void setID() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
