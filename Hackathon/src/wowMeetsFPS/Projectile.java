8c6c3d02855386d2aa30265aa35f8b51b74a2ece
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2017-11-04 16:18:15 -0400

    Projectile Class
    -Image
    -Stats
    -Calculate unitVector gets unit vector
    -move uses unitvector*speed to add to position

--- Hackathon/src/wowMeetsFPS/Projectile.java
+++ Hackathon/src/wowMeetsFPS/Projectile.java
@@ -1,5 +1,35 @@
 package wowMeetsFPS;
 
-public interface Projectile {
+import java.awt.geom.Point2D;
+
+public class Projectile {
  //Interface to determine the projectiles of a character
+	int range;
+	int damage;
+	int speed;
+	Point2D position;
+	Point2D unitVector; //This is the direction the projectile travels. Magnitude (sqrt(x^2 + y^2)) must = 1
+	//
+	public Projectile(int range, int damage, int speed, Point2D position) {
+		this.range = range;
+		this.damage = damage;
+		this.speed = speed;
+	}
+	public void move() {
+		Point2D distTrav = new Point2D.Double(unitVector.getX()*speed, unitVector.getY()*speed);
+		position = new Point2D.Double(position.getX() + distTrav.getX(), position.getY() + distTrav.getY());
+	}
+	public Point2D getUnitV() {
+		return unitVector;
+	}
+	public Point2D getPos() {
+		return position;
+	}
+	public Point2D calcUnitVector(Point2D.Double mouseClick, Point2D playerPos) {
+	
+		Point2D directionVector = new Point2D.Double(mouseClick.getX() - playerPos.getX(), mouseClick.getY() - mouseClick.getY());
+		double mag = Math.sqrt((Math.pow(directionVector.getX(), 2)) + Math.pow(directionVector.getY(), 2));
+		Point2D unitV = new Point2D.Double(directionVector.getX()/mag, directionVector.getY()/mag);
+		return unitV;
+	}
 }
