e9c53241cfb7ed14280044621dc27e6920557a39
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2017-11-04 12:34:16 -0400

    Map
    -added some variables and stuff

--- Hackathon/src/wowMeetsFPS/Map.java
+++ Hackathon/src/wowMeetsFPS/Map.java
@@ -1,10 +1,13 @@
 package wowMeetsFPS;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -15,21 +18,26 @@ public class Map extends JPanel implements ActionListener{
 	
 	private int delay = 5; //sets time between frames 
 	private Timer spf = new Timer(delay, this); //seconds per frame. After x seconds the painting will refresh and update graphics
-	
+	private Image background;
 	
 	public Map(Color c) {
 		addKeyListener(new KeyAction());
+		setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+		
 	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	}
-	public void doDrawing(Graphics g) {
+	public void doDrawing(Graphics g) { //does the drawing
+		//g.drawImage(background, 0, 0, null);
 		
 	}
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent arg0) { //executes actions here
+
 		
 	}
+	public int getHeight() { return B_HEIGHT; }
+	public int getWidth() { return B_WIDTH; }
 }
