f175df6b505f7f86c0527e4a1e93ff6e1403c021
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2017-11-04 17:20:36 -0400

    image + projectile

--- Hackathon/src/wowMeetsFPS/Projectile.java
+++ Hackathon/src/wowMeetsFPS/Projectile.java
@@ -7,13 +7,14 @@ public class Projectile {
 	int range;
 	int damage;
 	int speed;
-	Point2D position;
-	Point2D unitVector; //This is the direction the projectile travels. Magnitude (sqrt(x^2 + y^2)) must = 1
+	Point2D.Double position;
+	Point2D.Double unitVector; //This is the direction the projectile travels. Magnitude (sqrt(x^2 + y^2)) must = 1
 	//
-	public Projectile(int range, int damage, int speed, Point2D position) {
+	public Projectile(int range, int damage, int speed, Point2D.Double position, Point2D.Double target) {
 		this.range = range;
 		this.damage = damage;
 		this.speed = speed;
+		this.unitVector = calcUnitVector(target, position);
 	}
 	public void move() {
 		Point2D distTrav = new Point2D.Double(unitVector.getX()*speed, unitVector.getY()*speed);
@@ -25,11 +26,11 @@ public class Projectile {
 	public Point2D getPos() {
 		return position;
 	}
-	public Point2D calcUnitVector(Point2D.Double mouseClick, Point2D playerPos) {
+	public Point2D.Double calcUnitVector(Point2D.Double destination, Point2D.Double start) {
 	
-		Point2D directionVector = new Point2D.Double(mouseClick.getX() - playerPos.getX(), mouseClick.getY() - mouseClick.getY());
+		Point2D directionVector = new Point2D.Double(destination.getX() - start.getX(), destination.getY() - start.getY());
 		double mag = Math.sqrt((Math.pow(directionVector.getX(), 2)) + Math.pow(directionVector.getY(), 2));
-		Point2D unitV = new Point2D.Double(directionVector.getX()/mag, directionVector.getY()/mag);
+		Point2D.Double unitV = new Point2D.Double(directionVector.getX()/mag, directionVector.getY()/mag);
 		return unitV;
 	}
 }
