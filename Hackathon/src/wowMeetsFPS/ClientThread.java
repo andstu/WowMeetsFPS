50dd7585471932ca5632a20ba0a6d65ef99c616a
UnitedFailures <mfclinton0@gmail.com>
2017-11-04 13:24:07 -0400

    Base Client and Data Finished
    I finished the framework for the client and data classes.

---
+++ Hackathon/src/wowMeetsFPS/ClientThread.java
@@ -0,0 +1,35 @@
+package wowMeetsFPS;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientThread implements Runnable{
+
+	protected Socket socket;
+	protected Character user;
+	
+	public ClientThread(Socket socket, Character user) {
+		this.socket = socket;
+		this.user = user;
+	}
+	
+	@Override
+	public void run() {
+		
+		try {
+			ObjectOutputStream infoToServer = new ObjectOutputStream(socket.getOutputStream());
+			infoToServer.writeObject(new Data<Character>(user));
+			
+			ObjectInputStream infoFromServer = new ObjectInputStream(socket.getInputStream());
+			Data<Character[]> otherPlayers = (Data<Character[]>) infoFromServer.readObject();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- Hakcatho/src/common/Data.java
+++ Hakcatho/src/common/Data.java
@@ -2,8 +2,20 @@ package common;
 
 import java.io.Serializable;
 
-public class Data implements Serializable{
+public class Data<T> implements Serializable{
 
+	T info;
 	
+	//Creates a wrapper class holding a generic type of information
+	Data(T storedInformation)
+	{
+		info = storedInformation;
+	}
+	
+	//Gets the information from the wrapper class
+	T getInfo()
+	{
+		return info;
+	}
 	
 }
