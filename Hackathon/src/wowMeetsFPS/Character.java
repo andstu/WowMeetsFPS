d544b311189372952a8b8be7e981438cdfbf8077
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2017-11-04 01:55:45 -0400

    Character Interface
    Added methods to Character Interface class

--- Hackathon/src/wowMeetsFPS/Character.java
+++ Hackathon/src/wowMeetsFPS/Character.java
@@ -1,5 +1,34 @@
 package wowMeetsFPS;
 
-public interface Character {
+import java.awt.Point;
 
+import javax.swing.ImageIcon;
+
+public interface Character {
+	
+	/*
+	 * Moves by Updating Pos
+	 * Add speed to Pos
+	 */
+	public void move();
+	/*
+	 * Attack using projectiles
+	 * Should have a linklist to store projectiles
+	 * cooldown so no spamming
+	 */
+	public void attack();
+	
+	public int getHP();
+	public int getDefense();
+	public int getAttack();
+	public int getSpeed();
+	public ImageIcon getImage();
+	public Point.Double getLoc();
+	
+	public void setHP(int hp);
+	public void setDefense(int defense);
+	public void setAttack(int attack);
+	public void setSpeed(int speed);
+	public ImageIcon setImage(ImageIcon sprite);
+	public Point.Double setLoc(Point.Double location);
 }
