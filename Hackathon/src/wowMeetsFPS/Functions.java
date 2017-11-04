464f1d653e68fbec25963d635cdb7745dcc79d2d
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2017-11-04 16:47:17 -0400

    Function Class
    To check if two object collide

---
+++ Hackathon/src/wowMeetsFPS/Functions.java
@@ -0,0 +1,20 @@
+package wowMeetsFPS;
+
+import java.awt.geom.Point2D;
+
+public class Functions {
+
+	public boolean collides(Point2D.Double aTL, Point2D.Double aBR, Point2D.Double bTL, Point2D.Double bBR) {
+		/*if(aTL.getX().compareTo(bBR.getX()) > 0 || bTL.getX().compareTo(aBR.getX()) > 0){
+			
+			return false;
+		}
+		if(aTL.getY().compareTo(bBR.getY()) > 0 || bTL.getY().compareTo(aBR.getY()) > 0){
+			
+			return false;
+		}
+		return true;
+		*/
+		return false;
+	}
+}
