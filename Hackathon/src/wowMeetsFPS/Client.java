58b686168d973b37feea307a4c1f07ea026168f4
andstu <atlightning11@gmail.com>
2017-11-04 22:42:39 -0400

    IP Fix

--- Hackathon/src/wowMeetsFPS/Client.java
+++ Hackathon/src/wowMeetsFPS/Client.java
@@ -9,12 +9,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Client {
+	final static int PORT = 25565;
+	final static String ip = "34.214.63.87";
+	
 	
 	String username;
 	static Character[] otherPlayers;
 	static Character user;
 	static Map map;
-	final static int PORT = 25565;
 	static Timer timer = new Timer();
 	static TimerTask tasks = new TimerTask() {
 		@Override
@@ -26,7 +28,7 @@ public class Client {
 
 			    @Override
 			    public void run() {
-			         connectToServer("127.0.0.1",PORT, user);       
+			         connectToServer(ip,PORT, user);       
 			    }
 			            
 			});
